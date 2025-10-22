6b84416af2dc333d8dcb999e765d66bfbc853aa3
tansan7271 <tansan7271@naver.com>
2025-10-22 15:39:56 +0900

    feat: 데이터 모델 추가 및 BusStop 패치 명령어 추가, 관리자 데이터베이스 조회 페이지 추가

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HangJeongDong, BusStop, BusData
 
 # Register your models here.
+admin.site.register(HangJeongDong)
+admin.site.register(BusStop)
+admin.site.register(BusData)
