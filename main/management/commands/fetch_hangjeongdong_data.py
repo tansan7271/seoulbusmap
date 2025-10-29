e7b2fd89ef079c913f30e91873c1f57d5ecd0a3d
tansan7271 <tansan7271@naver.com>
2025-10-29 09:43:48 +0900

    misc: 프론트엔드 코드 실수 1차 복구

--- main/management/commands/fetch_hangjeongdong_data.py
+++ main/management/commands/fetch_hangjeongdong_data.py
@@ -1,22 +1,28 @@
-from .base_command import BaseFetchCommand
+import requests
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from main.models import HangJeongDong, HangJeongDongHistory
 from main.utils import fetch_hangjeongdong_data_from_api, parse_hangjeongdong_data, save_hangjeongdong_data
 
-class Command(BaseFetchCommand):
-    service_name = 'fetch_hangjeongdong_data'
+class Command(BaseCommand):
     help = '서울시 행정동 이름/코드 및 평균 인구수를 수집하고 아카이빙합니다.'
 
-    def fetch(self, api_keys):
-        """API로부터 행정동 원본 데이터를 수집합니다."""
-        return fetch_hangjeongdong_data_from_api(api_keys['seoul'], self.stdout, self.style)
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('[fetch_hangjeongdong_data] 행정동 정보 수집 및 아카이빙을 시작합니다...'))
 
-    def parse(self, fetch_result):
-        """수집된 원본 데이터를 파싱하여 처리 가능한 형태로 변환합니다."""
-        return parse_hangjeongdong_data(fetch_result, self.stdout, self.style)
+        try:
+            seoul_api_key = settings.SEOUL_API_KEY
 
-    def save(self, parsed_data, api_keys):
-        """파싱된 데이터를 데이터베이스에 저장합니다."""
-        # 파싱된 데이터가 없을 경우 저장을 건너뜀
-        if not parsed_data or (not parsed_data[0] and not parsed_data[1]):
-            self.stdout.write(self.style.WARNING(f'[{self.service_name}] 파싱된 데이터가 없어 저장을 건너뜁니다.'))
-            return
-        save_hangjeongdong_data(parsed_data, self.stdout, self.style)
+            # 1. Fetch Data
+            raw_data = fetch_hangjeongdong_data_from_api(seoul_api_key, self.stdout, self.style)
+
+            # 2. Parse Data
+            parsed_data = parse_hangjeongdong_data(raw_data, self.stdout, self.style)
+
+            # 3. Save Data
+            save_hangjeongdong_data(parsed_data, self.stdout, self.style)
+
+            self.stdout.write(self.style.SUCCESS('[fetch_hangjeongdong_data] 모든 작업이 성공적으로 완료되었습니다.'))
+
+        except Exception as e:
+            raise CommandError(f'[fetch_hangjeongdong_data] 전체 작업 중 오류 발생: {e}')
