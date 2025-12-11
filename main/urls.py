085ff13abbcddff2d18359c2a72bdacff627e6bd
jimin-03 <jjimin1024@gmail.com>
2025-12-11 16:19:32 +0900

    feat: 000 1차

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('hangjeongdong/<str:hjd_code>/', views.hangjeongdong_detail, name='hangjeongdong_detail'),
     path('hangjeongdong/<str:hjd_code>/busstop/<str:busstop_id>/', views.busstop_detail, name='busstop_detail'),
     path('analysis/', views.analysis_report, name='analysis_report'),
+    path('000/', views.000_visualization, name='000_visualization'),
 ]
